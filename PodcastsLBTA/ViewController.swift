a15a720465e33ffa666d02749172fd3f896e917e
Renald <renksss@gmail.com>
2019-10-30 15:46:42 +0300

    Initial Commit

---
+++ PodcastsLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodcastsLBTA
+//
+//  Created by renks on 30/10/2019.
+//  Copyright © 2019 Renald Renks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
